a357734febb47d3bdea47c4818a6e08d08006ec6
hmanny <harry@smarterwebcompany.co.uk>
2015-07-16 17:33:03 +0100

    sharp all  but footer non responsive

---
+++ js/twitterfetcherfunction.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){ 
+            var config2 = {
+                      "id": '608294831014748160',
+                      "domId": 'tweets',
+                      "maxTweets": 1,
+                      "enableLinks":true,
+                      "showUser": false,
+                      "showTime": true,
+                      "showImages": false
+                };       
+                twitterFetcher.fetch(config2);
+
+            });
